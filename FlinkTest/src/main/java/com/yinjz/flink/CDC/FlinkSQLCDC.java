b59522e3b6a1dfe51fe8fac42643ad46a186e43c
Loya123-123 <exiaozhong@163.com>
2023-08-02 10:35:20 +0800

    20230801提交更新

--- FlinkTest/src/main/java/com/yinjz/flink/CDC/FlinkSQLCDC.java
+++ FlinkTest/src/main/java/com/yinjz/flink/CDC/FlinkSQLCDC.java
@@ -1,11 +1,9 @@
 package com.yinjz.flink.CDC;
 
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.types.Row;
+
 
 public class FlinkSQLCDC {
 
@@ -15,29 +13,29 @@ public class FlinkSQLCDC {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+//                "'url' = 'jdbc:mysql://localhost:3306/test?useSSL=false&useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC'," +
 
         //2.使用FLINKSQL DDL模式构建CDC 表
-        tableEnv.executeSql("CREATE TABLE test ( " +
-                " id STRING primary key, " +
-                " name STRING " +
-                ") WITH ( " +
-                " 'connector' = 'mysql-cdc', " +
-                " 'scan.startup.mode' = 'latest-offset', " +
-                " 'hostname' = 'localhost', " +
-                " 'port' = '3306', " +
-                " 'username' = 'root', " +
-                " 'password' = '12345678', " +
-                " 'database-name' = 'test', " +
-                " 'table-name' = 'test' " +
-                ")");
-
+        tableEnv.executeSql("create table test (" +
+                "id String primary Key ," +
+                "name String " +
+                ") WITH (" +
+                "'connector' = 'mysql-cdc'," +
+                "'scan.startup.mode' = 'latest-offset'," +
+                "'hostname' = 'localhost'," +
+                "'port' = '3306'," +
+                "'username' = 'root'," +
+                "'password' = '12345678'," +
+                "'database-name' = 'test'," +
+                "'table-name' = 'test' )");
+        String sql = "select id,name from test" ;
         //3.查询数据并转换为流输出
-        Table table = tableEnv.sqlQuery("select * from test");
-        DataStream<Tuple2<Boolean, Row>> retractStream = tableEnv.toRetractStream(table, Row.class);
-        retractStream.print();
+        Table table = tableEnv.sqlQuery(sql);
 
+        table.printSchema();
+        tableEnv.toChangelogStream(table).print(" ");
         //4.启动
-        env.execute("FlinkSQLCDC");
+        env.execute(" ");
 
     }
 
